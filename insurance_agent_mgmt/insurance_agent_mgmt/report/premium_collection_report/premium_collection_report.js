759d37e99929eaef7fdbe5febdd35cae3d59c6ed
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-06-12 11:34:32 +0530

    feat: add JS filter files for Premium Collection and Commission Summary reports
    Add missing frappe.query_reports filter configurations to match
    the existing Lead Conversion Report pattern.

---
+++ insurance_agent_mgmt/insurance_agent_mgmt/report/premium_collection_report/premium_collection_report.js
@@ -0,0 +1,22 @@
+frappe.query_reports["Premium Collection Report"] = {
+    filters: [
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.now_date(), -1),
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.now_date(),
+        },
+        {
+            fieldname: "agent",
+            label: __("Agent"),
+            fieldtype: "Link",
+            options: "Insurance Agent",
+        },
+    ],
+};
